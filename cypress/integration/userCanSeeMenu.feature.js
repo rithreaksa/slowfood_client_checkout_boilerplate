ec1507dd8d9cfa6a0289b673b45508b8c4140028
Rithreaksa <wonderfulreaksa@gmail.com>
2020-12-04 11:08:55 +0100

    WIP with refactor class component to react hook

--- cypress/integration/userCanSeeMenu.feature.js
+++ cypress/integration/userCanSeeMenu.feature.js
@@ -1,42 +1,40 @@
 /* eslint-disable no-undef */
-describe('user views menus', () => {
-
+describe("user views menus", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3001")
+    cy.visit("http://localhost:3001");
   });
 
-  describe('if there are products', () => {
+  describe("if there are products", () => {
     before(() => {
       cy.server();
       cy.route({
-        method: 'GET',
-        url: 'http://localhost:3000/api/products',
-        response: 'fixture:product_data.json'
-      })
-    })
+        method: "GET",
+        url: "http://localhost:3000/api/products",
+        response: "fixture:product_data.json",
+      });
+    });
 
-    it('successfully', () => {
-      cy.get('#index').within(() => {
-        cy.contains('Spaghetti and Meatballs')
-        cy.contains('Salad')
-        cy.contains('Ice Cream')
-      })
-    })
+    it("successfully", () => {
+      cy.get("#index").within(() => {
+        cy.contains("Spaghetti and Meatballs");
+        cy.contains("Salad");
+        cy.contains("Ice Cream");
+      });
+    });
   });
 
-  describe('when the are NO products', () => {
-
+  describe("when the are NO products", () => {
     before(() => {
       cy.server();
       cy.route({
-        method: 'GET',
-        url: 'http://localhost:3000/api/products',
-        response: []
-      })
-    })
+        method: "GET",
+        url: "http://localhost:3000/api/products",
+        response: { "products": [] },
+      });
+    });
 
-    it('unsuccessfully', () => {
-      cy.get('#index').should('not.exist')
-    })
+    it("unsuccessfully", () => {
+      cy.get("#index").should("not.exist");
+    });
   });
-})
+});
--- src/components/DisplayProductData.jsx
+++ src/components/DisplayProductData.jsx
@@ -1,46 +1,74 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { getData } from "../modules/productData";
 
-class DisplayProductData extends Component {
-  state = {
-    productData: [],
+const DisplayProductData = () => {
+  let [productData, setProductData] = useState([]);
+  const getProductData = async () => {
+    let result = await getData();
+    setProductData(result.data.products);
   };
 
-  componentDidMount() {
-    this.getProductData();
-  }
+  useEffect(() => {
+    // it mimics componentDidMount
+    getProductData();
+  }, []);
 
-  async getProductData() {
-    let result = await getData();
-    this.setState({ productData: result.data.products });
-  }
-
-  render() {
-    let dataIndex;
-    if (
-      Array.isArray(this.state.productData) &&
-      this.state.productData.length
-    ) {
-      dataIndex = (
-        <div id="index">
-          {this.state.productData.map((item) => {
-            return (
-              <div key={item.ide} data-cy={`product-${item.id}`}>
-                {item.name}
-                {item.description}
-                {item.price}{" "}
-                {localStorage.getItem("authenticated") === "true" && (
-                  <button>Add to Order</button>
-                )}
-              </div>
-            );
-          })}
-        </div>
-      );
-    }
-
-    return <div>{dataIndex}</div>;
-  }
-}
-
+  let dataIndex = productData.map((item) => {
+    return (
+      <div key={item.ide} data-cy={`product-${item.id}`}>
+        {item.name}
+        {item.description}
+        {item.price}{" "}
+        {localStorage.getItem("authenticated") === "true" && (
+          <button>Add to Order</button>
+        )}
+      </div>
+    );
+  });
+  return <>{productData.length > 0 && <div id="index">{dataIndex}</div>}</>;
+};
 export default DisplayProductData;
+
+// class DisplayProductData extends Component {
+//   state = {
+//     productData: [],
+//   };
+
+//   componentDidMount() {
+//     this.getProductData();
+//   }
+
+//   async getProductData() {
+//     let result = await getData();
+//     this.setState({ productData: result.data.products });
+//   }
+
+//   render() {
+//     let dataIndex;
+//     if (
+//       Array.isArray(this.state.productData) &&
+//       this.state.productData.length
+//     ) {
+//       dataIndex = (
+// <div id="index">
+//   {this.state.productData.map((item) => {
+//     return (
+//       <div key={item.ide} data-cy={`product-${item.id}`}>
+//         {item.name}
+//         {item.description}
+//         {item.price}{" "}
+//         {localStorage.getItem("authenticated") === "true" && (
+//           <button>Add to Order</button>
+//         )}
+//       </div>
+//             );
+//           })}
+//         </div>
+//       );
+//     }
+
+//     return <div>{dataIndex}</div>;
+//   }
+// }
+
+// export default DisplayProductData;
